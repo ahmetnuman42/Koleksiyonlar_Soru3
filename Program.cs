131cb3239a720db5e73247e9f58a11c3d8c92b97
ahmettnuman42 <ahmetnumanecevitci@gmail.com>
2022-09-10 20:43:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cümle giriniz=");
+            string cumle = Console.ReadLine();
+            string sesliharfler = "aeıioöuü";
+            ArrayList cumledekiSesliHarfler = new ArrayList();
+            cumle.ToLower();
+            if (!string.IsNullOrEmpty(cumle))
+            {
+                for(int i = 0; i < cumle.Length; i++)
+                {
+                    if (sesliharfler.Contains(cumle[i]))
+                    {
+                        cumledekiSesliHarfler.Add(cumle[i]);
+                    }
+                }
+            }
+            Console.WriteLine("Cumledeki sesli harfler= ");
+            cumledekiSesliHarfler.Sort();
+            foreach (var item in cumledekiSesliHarfler)
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+    }
+}
